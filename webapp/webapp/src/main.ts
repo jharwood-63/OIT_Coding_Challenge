ee48119723cb3460226d7134d5214a0a2add31be
jharwood-63 <83321059+jharwood-63@users.noreply.github.com>
2024-01-20 17:50:50 -0700

    Changed from string to url object

--- webapp/webapp/src/main.ts
+++ webapp/webapp/src/main.ts
@@ -21,10 +21,12 @@ registerPlugins(app)
 app.mount('#app')
 
 async function getMovies(searchParam: string) {
-  const url = `http://localhost:8080/movies?search=${searchParam}`;
+  const urlString = `http://localhost:8080/movies?search=${searchParam}`;
+  const url = new URL(urlString);
+  console.log(url.href)
 
   try {
-    const response = await axios.get(url);
+    const response = await axios.get(url.href);
 
     return response.data;
   } catch (error: any) {
